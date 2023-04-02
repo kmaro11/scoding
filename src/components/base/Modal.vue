a38f4f048c2ba7ac96e82b826cd745880afff0a7
Rokas <rokas.kmaro@gmail.com>
2023-04-02 15:26:32 +0300

    added modal

---
+++ src/components/base/Modal.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    default: "<<Title goes here>>",
+  },
+})
+const emit = defineEmits(['closeModal'])
+
+const closeModal = () => {
+  emit('closeModal')
+}
+
+</script>
+
+<template>
+  <div class="modal fade show"
+       tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true" role="dialog"
+       style="display:block">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">{{ title }}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="closeModal"></button>
+        </div>
+        <div class="modal-body">
+          <slot name="body" />
+        </div>
+        <div class="modal-footer">
+          <slot name="footer"></slot>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="closeModal">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import Modal from "@/components/base/Modal.vue";
+
+const openTaskModal = ref(false)
 </script>
 
 <template>
   <main>
     <div class="container py-4 px-3 mx-auto">
-      kazkas
+      <button type="button" class="btn btn-primary" @click="openTaskModal = true">Open modal</button>
     </div>
+
+    <Modal v-if="openTaskModal" @close-modal="openTaskModal = false" title="Add task">
+      <template #body>
+        <div>
+
+        </div>
+      </template>
+    </Modal>
   </main>
 </template>
